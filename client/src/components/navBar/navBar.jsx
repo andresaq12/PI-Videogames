d8abe4f1aea05096675c9be0ebda0edf9ca2236e
Andres Alvarez <aalvarezqu@gmail.com>
2022-01-28 19:37:12 -0500

    Update CSS navBar

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -4,21 +4,33 @@ import styles from '../navBar/navBar.module.css'
 const NavBar = () => {
 
   return (
-    <>
+    <header>
       <nav className={styles.topnav}>
-        <div className={styles.container}>
-          <ul>
-            <li>
-              <Link to='/home' style={{ textDecoration: 'none' }}> Home</Link>
-            </li>
-            <li>
-              <Link to='/game/add' style={{ textDecoration: 'none' }}>Add Videogame</Link>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            <Link to='/home' className={styles.link}> Home</Link>
+          </li>
+          <li>
+            <Link to='/game/add'>Add Videogame</Link>
+          </li>
+        </ul>
       </nav>
-    </ >
+    </header >
   )
 }
 
 export default NavBar
+
+  // < header >
+  // <nav className={styles.topnav}>
+  //   <div className={styles.container}>
+  //     <ul>
+  //       <li>
+  //         <Link to='/home' style={{ textDecoration: 'none' }}> Home</Link>
+  //       </li>
+  //       <li>
+  //         <Link to='/game/add' style={{ textDecoration: 'none' }}>Add Videogame</Link>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // </nav>
